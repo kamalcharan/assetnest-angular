d6904069b5282a374343838cd6cd42018fd3dde9
arun <arun@hivebox.in>
2019-08-28 10:36:14 +0530

    changes for build

--- src/app/view-apikeys/view-apikeys.component.ts
+++ src/app/view-apikeys/view-apikeys.component.ts
@@ -69,4 +69,23 @@ export class ViewApikeysComponent {
     
     })
   }
+  copyMessage(val: string){
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
+
+  ChangeAPIStatus(item)
+  {
+    
+  }
 }
